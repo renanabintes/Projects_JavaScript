ae8977a9c7eab69ef56539b10f8c5f38ed12a065
renanabintes <rcabintes@gmail.com>
2021-10-06 22:49:52 -0300

    Atualização

---
+++ Project Dice Game/Project_Dice_Game.js
@@ -0,0 +1,11 @@
+// • Perguntar quantas rodadas você quer fazer;
+// • Perguntar quantos jogadores vão jogar;
+// • Criar um objeto pra cada jogador com nome e número tirado;
+// • Guarda todos os objetos em uma lista;
+// • Ordenar esses objetos, sabendo que o vencedor tirou o maior número
+// no dado.
+// • Mostrar no final qual jogador ganhou mais rodadas e foi o grande
+// campeão.
+
+const prompt = require('prompt-sync')();
+
