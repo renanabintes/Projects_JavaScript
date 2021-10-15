7e1416faf74416b7687d986f2ad4b3f54677f627
renanabintes <rcabintes@gmail.com>
2021-10-14 21:51:36 -0300

    Atualização

--- Project Dice Game/Project_Dice_Game.js
+++ Project Dice Game/Project_Dice_Game.js
@@ -1,25 +1,23 @@
-// • Perguntar quantas rodadas você quer fazer;
-// • Perguntar quantos jogadores vão jogar;
-// • Criar um objeto pra cada jogador com nome e número tirado;
-// • Guarda todos os objetos em uma lista;
-// • Ordenar esses objetos, sabendo que o vencedor tirou o maior número
-// no dado.
-// • Mostrar no final qual jogador ganhou mais rodadas e foi o grande
-// campeão.
-
 const prompt = require('prompt-sync')();
 
 let rodadas = +prompt("Informe o número de rodadas: ");
-for (let i = 0; i < rodadas; i++) {}
+let qtdJogadores = +prompt("Informe a quantidade de jogadores: ");
+let jogador = {};
 let infJogo = [];
-let qtdJogadores = +prompt("Informe a quantidade de jogadores: ")
-for (let i = 0; i < qtdJogadores; i++) {
-    let jogador = {};
+for (let i = 0; i < rodadas; i++) {
     jogador.nome = prompt("Informe o nome do jogador: ");
-    jogador.numDado = Math.floor(Math.random() * 6 + 1);
+    numero = Math.floor(Math.random() * 6 + 1);
+}   
+let jogadas = [];
+for (let j = 0; j < qtdJogadores; j++) {
+    jogador.dado = jogadas;
+    jogadas.push(numero);
     infJogo.push(jogador);
 }
 infJogo.sort((a, b) => {
-    return b.numDado - a.numDado;
-})
+    return b.nome - a.nome;
+});
 console.log(infJogo);
+
+console.log("\n O campeão é "+ infJogo[0]["nome"]);
+
