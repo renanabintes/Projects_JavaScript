e6bcf232f891deca15c69a416686df3bc67465d1
renanabintes <rcabintes@gmail.com>
2021-10-18 19:23:18 -0300

    Em consturção...

---
+++ Projeto Final - Jogo de Ficção Interativa/index.js
@@ -0,0 +1,9 @@
+// 1. Sistema de escolhas e consequências (2 pontos)
+// 2. Pelo menos 10 consequências baseadas nas escolhas (2 pontos)
+// 3. Mecanismo de controle do dia do personagem (1 ponto)
+// 4. Cada escolha avança no tempo (1 ponto)
+// 5. Utilização das declarações: 
+// Laços de repetição (1 ponto)
+// Funções (1 ponto)
+// Condicionais (1 ponto)
+// Objetos (1 ponto)
