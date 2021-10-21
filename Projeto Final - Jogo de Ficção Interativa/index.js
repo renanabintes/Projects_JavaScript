b84d3f7de745bc65067a77c171208e50d2bcf85d
renanabintes <rcabintes@gmail.com>
2021-10-21 17:26:38 -0300

    Revisar...

--- Projeto Final - Jogo de Ficção Interativa/index.js
+++ Projeto Final - Jogo de Ficção Interativa/index.js
@@ -1,13 +1,3 @@
-// 1. Sistema de escolhas e consequências (2 pontos)
-// 2. Pelo menos 10 consequências baseadas nas escolhas (2 pontos)
-// 3. Mecanismo de controle do dia do personagem (1 ponto)
-// 4. Cada escolha avança no tempo (1 ponto)
-// 5. Utilização das declarações: 
-// Laços de repetição (1 ponto)
-// Funções (1 ponto)
-// Condicionais (1 ponto)
-// Objetos (1 ponto)
-
 const prompt = require('prompt-sync')();
 
 console.log("Bem vindos ao Jogo Campo de Batalha  - (Viva ou Morra Lutando)");
